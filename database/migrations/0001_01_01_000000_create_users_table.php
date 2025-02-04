96b70d574fa20e533c17a2f188844d27bc6f0545
17Jared <jaredmutua17@gmail.com>
2025-02-04 15:02:48 +0300

    Authentication(edit1)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('phone')->unique();
             $table->string('email')->unique();
-            $table->integer('phone_number');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
